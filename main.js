f54d3bf70d592034327ee8980ab231043cf6c1d0
1977Suryaa <101641242+1977Suryaa@users.noreply.github.com>
2022-11-21 17:59:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+function preload(){
+    song1=loadSound("coffin.mp3")
+    song2=loadSound("Pablo.mp3")
+}
 function setup(){
     canvas=createCanvas(300,300)
     canvas.position(500,125)
-
+    webcam=createCapture(VIDEO)
+    webcam.hide()
+    webcam.size(300,300)
+}
+function draw(){
+    image(webcam,0,0,300,300)
 }
